e294edcfd6eaac9af0cd15e094733ecd3798ecc4
Eddy Mwai <105627607+EddyMwai@users.noreply.github.com>
2024-07-15 11:28:03 +0300

    Add files via upload

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -65,6 +65,61 @@ class AdminController extends Controller
         return redirect()->back();
     }
 
+         public function registerUser1(Request $request){
+        $user = new user;
+        $file = \Request::file('image');
+        $destination = 'user_images';
+        $ext= $file->getClientOriginalExtension();
+        $mainFilename = Str::random(40).date('h-i-s').".".$ext;
+        $file->move($destination, $mainFilename);
+
+        $name = $request->input('fname');
+        $phone_number = $request->input('phone');
+        // $user_type = $request->input('user_type');
+        $user_type = 'farmer';
+        $email = $request->input('email');
+        $password=Hash::make($request->input('password'));
+
+        $user->profile_photo_path=$mainFilename;
+        $user->name=$name;
+        $user->phone_number=$phone_number;
+        $user->user_type=$user_type;
+        $user->email=$email;
+        $user->password=$password;
+
+        $user->save();
+
+        return view('users.home');
+    }
+
+         public function updateUser(Request $request){
+        $id = $request->input('uid');
+        $user = user::find($id);
+        $file = \Request::file('image');
+        $destination = 'user_images';
+        $ext= $file->getClientOriginalExtension();
+        $mainFilename = Str::random(40).date('h-i-s').".".$ext;
+        $file->move($destination, $mainFilename);
+
+        $name = $request->input('fname');
+        $phone_number = $request->input('phone');
+        // $user_type = $request->input('user_type');
+        $user_type = 'farmer';
+        $email = $request->input('email');
+        $password=Hash::make($request->input('password'));
+
+        $user->profile_photo_path=$mainFilename;
+        $user->name=$name;
+        $user->phone_number=$phone_number;
+        $user->user_type=$user_type;
+        $user->email=$email;
+        $user->password=$password;
+
+        $user->save();
+
+        return redirect()->back();
+    }
+
         public function deleteUser($id)
     {
         $data = User::findOrFail($id);
